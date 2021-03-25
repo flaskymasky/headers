d5351d09c5eb11452b82986f6a68d8f31dc61613
flaskymasky <53376718+flaskymasky@users.noreply.github.com>
2021-03-25 22:41:38 +0100

    Create full_socket.h

---
+++ full_socket.h
@@ -0,0 +1,38 @@
+#ifndef SOCKET_HEADER
+#define SOCKET_HEADER
+
+#include <sys/types.h>
+
+#include <sys/uio.h>
+
+#include <errno.h>
+
+#include <sys/ioctl.h>
+
+#include <fcntl.h>
+
+#include <sys/socket.h>
+
+#include <netdb.h>
+
+#include <netinet/in_systm.h>
+
+#include <netinet/ip_icmp.h>
+
+#include <netinet/udp.h>
+
+#include <netinet/ip.h>
+
+#include <netinet/in.h>
+
+#include <arpa/inet.h>
+
+#include <arpa/nameser.h>
+
+#include <resolv.h>
+
+#include <net/if.h>
+
+#include <strings.h>
+
+#endif
